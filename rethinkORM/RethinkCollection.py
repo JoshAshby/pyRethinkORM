936080e4db471097b0ec1df86ecd48a0696f4d79
JoshAshby <joshuaashby@joshashby.com>
2013-07-15 14:24:32 -0600

    hashing out an idea... not sure if final

--- rethinkORM/RethinkCollection.py
+++ rethinkORM/RethinkCollection.py
@@ -12,3 +12,37 @@ class RethinkCollection(object):
 
     def __init__(self, model, **kwargs):
         self.table = model
+
+    def join(self, model):
+        pass
+
+    def paginate(self, start,finish):
+        pass
+
+    @property
+    def currentPage(self):
+        pass
+
+    @property
+    def perpage(self):
+        pass
+
+    @property
+    def hasnextpage(self):
+        pass
+
+    @property
+    def pages(self):
+        pass
+
+    def orderBy(self, field):
+        pass
+
+    def __repr(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __contains__(self, item):
+        pass
