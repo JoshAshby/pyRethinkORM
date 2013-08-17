0f9b881c8a6a09e32c02931f364d6c5cda92e486
JoshAshby <joshuaashby@joshashby.com>
2013-08-17 15:22:00 -0600

    bump release date

--- rethinkORM/__init__.py
+++ rethinkORM/__init__.py
@@ -3,7 +3,7 @@ from rethinkModel import RethinkModel
 from rethinkCollection import RethinkCollection
 import tests
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ["RethinkModel",
