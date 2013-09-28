af4527a01f5a6642e8a6b74704eee13f16185bae
JoshAshby <joshuaashby@joshashby.com>
2013-09-27 22:11:19 -0600

    setup.py didn't like the a1 in the version... hmm

--- rethinkORM/__init__.py
+++ rethinkORM/__init__.py
@@ -3,7 +3,7 @@ from rethinkModel import RethinkModel
 from rethinkCollection import RethinkCollection
 import tests
 
-__version__ = '1.0.0a1'
+__version__ = '1.0.0'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ["RethinkModel",
