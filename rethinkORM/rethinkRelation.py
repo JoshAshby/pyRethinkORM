2f159a0ab85247370c46b02d44413f58bafc5e78
JoshAshby <joshuaashby@joshashby.com>
2013-10-23 13:00:40 -0600

    document rethinkRelation a little bit

--- rethinkORM/rethinkRelation.py
+++ rethinkORM/rethinkRelation.py
@@ -1,16 +1,47 @@
-import rethinkdb as r
 import rethinkModel as rm
 import rethinkCollection as rc
 
 
 class RethinkRelation(rm.RethinkModel):
-  has_one = []
+  """
+  Extension of the standard RethinkModel which allows for the creating of
+  one to one and one to many relationships between documents. Eventually this
+  will be merged into RethinkModel before the next release.
+
+  Documents have a relation set through setting and storing of `foreign_key`
+  which is simply the id of the parent document. This class takes care of
+  setting and working with that key to ensure that things run smoothly.
+  """
+
+  #has_one = []
   has_many = []
+  """
+  Defines a relationship of one parent to many children documents
+
+  Must be a list, and can be filled with classrefs for the models that
+  this model has many of. The name of the property on the parent object will
+  be the childs name, lowercased with an appended `s`
+
+  >>> class Comment:
+        table = "comments"
+
+  >>> class Post:
+        table = "posts"
+        has_many = [Comment]
+
+  >>> p = Post.create(title="Rising", body="The city of Atlantis comes to life\
+          as the special Stargate team arives in the gate room.")
+  >>> p.comments.all()
+  """
 
   def finish_init(self):
-    self.relationships()
+    self._build_relationships()
 
-  def relationships(self):
+  def _build_relationships(self):
+    """
+    Builds the relationships based off of the classrefs given in has_many and
+    has_one.
+    """
     self._set_keys_later = []
     def build_relation_class(wat, plural=False):
       data = {"foreign_key": self.id}
@@ -27,9 +58,9 @@ class RethinkRelation(rm.RethinkModel):
         self.protected_items = name
         self._set_keys_later.append(name)
 
-    if self.has_one is not None:
-      assert type(self.has_one) is list
-      build_relation_class(self.has_one)
+    #if self.has_one is not None:
+      #assert type(self.has_one) is list
+      #build_relation_class(self.has_one)
 
     if self.has_many is not None:
       assert type(self.has_many) is list
@@ -39,17 +70,21 @@ class RethinkRelation(rm.RethinkModel):
     if hasattr(self, "foreign_key"):
       self._data["foreign_key"] = self.foreign_key
 
-    if hasattr(self, "unique"):
-      pass # TODO: Do some fancy, arcanie, black magic to make this remain unique
+    #if hasattr(self, "unique"):
+      #pass # TODO: Do some fancy, arcanie, black magic to make this remain unique
 
     super(RethinkRelation, self).save()
 
     for key in self._set_keys_later:
       self._get(key).foreign_key = self.id
 
-
   @classmethod
   def all(cls):
+    """
+    Returns a RethinkCollection which represents all of documents within this
+    model, or all of the children if the model is attached to a parent through
+    has_many.
+    """
     if hasattr(cls, "foreign_key") and cls.foreign_key is not None:
       return rc.RethinkCollection(cls, {"foreign_key": cls.foreign_key})
     else:
@@ -57,5 +92,7 @@ class RethinkRelation(rm.RethinkModel):
 
   @classmethod
   def find(cls, field, value):
-      query = r.table(cls.table).filter({field: value}).coerce_to('array').run()
-      return query
+    """
+    Helper method to return a RethinkCollection with the given search.
+    """
+    return rc.RethinkCollection(cls, {field: value})
